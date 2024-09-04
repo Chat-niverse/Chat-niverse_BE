11d0a0ed8fe221f78cc0affc976f392d48a52683
Austin-Choi <austinsupra@gmail.com>
2024-09-05 01:28:59 +0900

    chore : initial commit

---
+++ src/main/java/com/be/chat_niverse/ChatNiverseApplication.java
@@ -0,0 +1,13 @@
+package com.be.chat_niverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatNiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatNiverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/chat_niverse/ChatNiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.chat_niverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatNiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
