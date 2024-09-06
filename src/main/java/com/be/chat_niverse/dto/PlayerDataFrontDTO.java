7eb927252172c8fa9f799dea95c82856d9f5f274
Austin-Choi <austinsupra@gmail.com>
2024-09-06 11:42:53 +0900

    feature : front랑 소통할 DTO 구현

---
+++ src/main/java/com/be/chat_niverse/dto/PlayerDataFrontDTO.java
@@ -0,0 +1,23 @@
+package com.be.chat_niverse.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class PlayerDataFrontDTO {
+    private String username; // 플레이어 닉네임
+    private String worldview; // 플레이어가 설정한 세계관
+    private String charsetting; // 캐릭터 설정
+    private String aim; // 플레이어 목표
+
+    private Map<String, Object> status; // 플레이어 현재 status
+    private int life; // 플레이어 life, 3으로 시작 0이 되면 게임오버
+    private Map<String, Object> inventory; // 플레이어 인벤토리
+    private String playlog; // 플레이어 플레이로그
+    private String gptsays; // 플레이어가 목표를 달성하면 보여줄 한줄 평
+
+    private String selectedchoice; // 플레이어가
+}
